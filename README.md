23a1d6737e0c91b90427e547fed4811d258b3d78
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-03-13 22:36:48 +0000

    update

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
-```pip install streamlit```
+# Système de Détection d'Intrusion Réseau
 
-```streamlit run app.py```
+Ce projet utilise des techniques d'apprentissage automatique pour identifier les tentatives d'intrusion dans les réseaux informatiques. Il s'appuie sur plusieurs algorithmes de Machine Learning pour évaluer les données du trafic réseau et déterminer si elles représentent une activité normale ou une tentative d'intrusion.
+
+## Caractéristiques Principales
+
+- Utilisation de cinq algorithmes différents : Régression Logistique, Random Forest, Naive Bayes, KNN, et SVC.
+- Prétraitement des données pour s'adapter aux exigences des modèles de Machine Learning.
+- Interface utilisateur intuitive développée avec Streamlit pour une interaction facile avec le système.
+- Capacité de traiter les données en temps réel via le téléchargement de fichiers CSV ou la saisie manuelle des caractéristiques.
+
+
+## Configuration de l'Environnement
+
+Pour exécuter ce projet, vous aurez besoin d'un environnement Python avec les bibliothèques suivantes :
+
+- pandas
+- joblib
+- scikit-learn
+- streamlit
+
+Vous pouvez installer ces dépendances en utilisant pip :
+
+```sh
+pip install pandas joblib scikit-learn streamlit
+
+Utilisation
+Pour exécuter l'application, naviguez dans le dossier du projet et lancez :
+
+
+streamlit run app.py
+
+Structure du Projet
+Structure du Projet
+app.py : Script principal de l'application Streamlit.
+./joblib/ : Dossier contenant les modèles pré-entraînés en format Joblib.
+Notebook-NIDS.ipynb
+Rapport-NIDS.pdf
+
+Contribution
+Les contributions à ce projet sont les bienvenues.
+
+Projet Machine Learning : Système de Détection d'Intrusion Réseau Cree par Zaid EL MOUADDIBE & Soulaimane Cherkaoui| ENSET 2024
--- app.py
+++ app.py
@@ -1,89 +1,116 @@
 import streamlit as st
-import numpy as np
-from joblib import load
 import pandas as pd
+from joblib import load
 
-# 1. Chargement des modèles et préprocesseurs
-models = {
-    'SVC': load('./joblib/SVC_model.joblib'),
-    'Random Forest': load('./joblib/Random_Forest_model.joblib'),
-    'Logistic Regression': load('./joblib/Logistic_Regression_model.joblib'),
-    'KNN': load('./joblib/KNN_model.joblib')
-}
-scaler = load('./joblib/scaler.joblib')
-label_encoders = load('./joblib/label_encoders.joblib')
+# Fonction pour charger les modèles
+def charger_modeles():
+    modeles = {
+        'Régression Logistique': load('./joblib/Logistic_Regression_model.joblib'),
+        'Random Forest': load('./joblib/Random_Forest_model.joblib'),
+        'Naive Bayes': load('./joblib/Naive_Bayes_model.joblib'),
+        'KNN': load('./joblib/KNN_model.joblib'),
+        'SVC': load('./joblib/SVC_model.joblib')
+    }
+    return modeles
 
-# Setup title and introduction
-st.title('Système de Détection d’Intrusion Réseau')
-st.markdown("""
-Bienvenue sur notre système de détecter les intrusions réseau utilisant le machine learning. Sélectionnez le modèle de prédiction, fournissez les données via un formulaire intuitif ou téléchargez un fichier CSV pour une analyse rapide.
-""")
+# Fonction pour prétraiter les données d'entrée
+def pretraiter_donnees_entree(input_df):
+    input_df['protocol_type'] = input_df['protocol_type'].map({'tcp': 0, 'udp': 1, 'icmp': 2}).fillna(0)
+    input_df['flag'] = input_df['flag'].map({'SF': 0, 'S0': 1, 'REJ': 2, 'RSTR': 3, 'RSTO': 4}).fillna(0)
+    
+    # Liste des colonnes nécessaires
+    colonnes_necessaires = [
+        'same_srv_rate', 'logged_in', 'dst_host_serror_rate',
+        'dst_host_same_srv_rate', 'dst_host_srv_count',
+        'dst_host_srv_serror_rate', 'flag', 'srv_serror_rate', 
+        'protocol_type', 'last_flag', 'serror_rate'
+    ]
+    
+    # Vérifier si toutes les colonnes nécessaires sont présentes
+    colonnes_manquantes = [col for col in colonnes_necessaires if col not in input_df.columns]
+    
+    if colonnes_manquantes:
+        for col in colonnes_manquantes:
+            input_df[col] = 0
 
-# Model selection
-model_choice = st.radio("Sélectionnez le modèle pour la prédiction:", list(models.keys()))
+    return input_df[colonnes_necessaires]
 
-# Choose between CSV upload and manual input
-option = st.radio("Méthode d'entrée des données:", ('Remplir manuellement les caractéristiques', 'Télécharger un fichier CSV'))
+# Fonction de prédiction qui choisit le modèle avec la probabilité la plus élevée
+def predire_avec_meilleur_modele(modeles, input_df):
+    meilleur_score = 0
+    meilleures_predictions = None
+    meilleures_proba_predictions = None
+    modele_choisi = None
+    for nom, modele in modeles.items():
+        if hasattr(modele, "predict_proba"):
+            proba_predictions = modele.predict_proba(input_df)[:, 1]
+            max_proba = max(proba_predictions)
+            if max_proba > meilleur_score:
+                meilleur_score = max_proba
+                predictions = modele.predict(input_df)
+                meilleures_predictions = predictions
+                meilleures_proba_predictions = proba_predictions
+                modele_choisi = nom
+    return meilleures_predictions, meilleures_proba_predictions, modele_choisi
 
-# Display CSV format instructions if CSV option is selected
-if option == 'Télécharger un fichier CSV':
+# Configuration de l'interface utilisateur Streamlit
+def configurer_interface_utilisateur():
+    st.title('Système de Détection d\'Intrusion Réseau')
     st.markdown("""
-    **Instructions pour le fichier CSV:**
-    - Le fichier doit contenir les colonnes pour les caractéristiques suivantes, dans l'ordre spécifié.
-    - Les noms des colonnes doivent correspondre exactement aux noms des caractéristiques.
-    - Les données doivent être prétraitées conformément aux exigences du modèle.
+    Ce projet utilise des techniques d'apprentissage automatique pour identifier les tentatives d'intrusion dans les réseaux informatiques. Il s'appuie sur cinq algorithmes différents : Régression Logistique, Random Forest, Naive Bayes, KNN et SVC. Le système évalue les données fournies et prédit si elles représentent une activité normale ou une attaque, en se basant sur le modèle qui donne la probabilité la plus élevée.
     """)
-    csv_file = st.file_uploader("Choisissez un fichier CSV", type=['csv'])
-    if csv_file is not None:
-        input_df = pd.read_csv(csv_file)
 
-# Manual feature input setup
-if option == 'Remplir manuellement les caractéristiques':
+def saisie_manuelle_caracteristiques():
     with st.form(key='feature_form'):
-        st.subheader('Veuillez entrer les valeurs pour les caractéristiques suivantes:')
-
-        # Define feature inputs with descriptions and appropriate widgets
-        feature_values = {
+        st.subheader('Veuillez entrer les valeurs pour les caractéristiques suivantes :')
+        # Les entrées pour les caractéristiques ici sont basées sur les 11 caractéristiques attendues par les modèles
+        valeurs_caracteristiques = {
+			
+			"logged_in": int(st.radio("Connecté ?", ('Oui', 'Non')) == 'Oui'),
+            "dst_host_srv_count": st.number_input("Nombre de connexions au service de l'hôte", min_value=0, max_value=100, value=50),
             "same_srv_rate": st.slider("Taux de connexions au même service", min_value=0.0, max_value=1.0, value=0.5, step=0.01),
-            "logged_in": st.radio("Est connecté ?", ('Oui', 'Non'), format_func=lambda x: '1' if x == 'Oui' else '0'),
-            "srv_serror_rate": st.slider("Taux d'erreurs de service", min_value=0.0, max_value=1.0, value=0.5, step=0.01),
-            "flag": st.selectbox("État de la connexion", options=label_encoders['flag'].classes_),
-            "dst_host_same_srv_rate": st.slider("Taux de connexion au même hôte de destination", min_value=0.0, max_value=1.0, value=0.5, step=0.01),
-            "dst_host_srv_count": st.number_input("Nombre de connexions au service de l'hôte de destination", min_value=0, max_value=100, value=50),
-            "serror_rate": st.slider("Taux d'erreurs de connexion", min_value=0.0, max_value=1.0, value=0.5, step=0.01),
-            "protocol_type": st.selectbox("Type de protocole", options=label_encoders['protocol_type'].classes_),
-            "last_flag": st.number_input("Dernier indicateur de la connexion", min_value=0, max_value=100, value=50),
-            "dst_host_serror_rate": st.slider("Taux d'erreurs de l'hôte de destination", min_value=0.0, max_value=1.0, value=0.5, step=0.01),
-            "dst_host_same_src_port_rate": st.slider("Taux de connexions au même port source de l'hôte", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+            "dst_host_same_srv_rate": st.slider("Taux de connexion au même hôte", min_value=0.0, max_value=1.0, value=0.5, step=0.01),
+            "srv_serror_rate": st.slider("Taux d'erreur de service", min_value=0.0, max_value=1.0, value=0.5, step=0.01),
+            "dst_host_same_src_port_rate": st.slider("Taux de connexions au même port source de l'hôte", min_value=0.0, max_value=1.0, value=0.5, step=0.01),
+            "dst_host_serror_rate": st.slider("Taux d'erreur de l'hôte de destination", min_value=0.0, max_value=1.0, value=0.5, step=0.01),
+            "last_flag": st.number_input("Dernier drapeau de la connexion", min_value=0, max_value=100, value=50),
+            "protocol_type": st.selectbox("Type de protocole", options=['tcp', 'udp', 'icmp']),
+            "flag": st.selectbox("État de la connexion", options=['SF', 'S0', 'REJ', 'RSTR', 'RSTO']),
+            "serror_rate": st.slider("Taux d'erreurs de connexion", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
         }
-        submit_button = st.form_submit_button(label='Prédire')
+        bouton_predire = st.form_submit_button('Prédire')
+        return pd.DataFrame([valeurs_caracteristiques]) if bouton_predire else None
 
-# Initialize a full feature vector with neutral values
-full_feature_vector = np.zeros(39)
+# Interface utilisateur pour le téléchargement de fichier CSV
+def upload_csv_interface():
+    uploaded_file = st.file_uploader("Ou téléchargez un fichier CSV", type=["csv"])
+    if uploaded_file is not None:
+        return pd.read_csv(uploaded_file)
+    return None
 
-# Data Preprocessing and Prediction
-def preprocess_and_predict(input_df, model_choice):
-    # Encode categorical features
-    for col, encoder in label_encoders.items():
-        if col in input_df:
-            input_df[col] = encoder.transform(input_df[col])
-    
-    # Scale features
-    scaled_features = scaler.transform(input_df)
-    
-    # Make prediction using the selected model
-    model = models[model_choice]
-    predictions = model.predict(scaled_features)
-    prediction_probabilities = model.predict_proba(scaled_features)[:, 1]
-    
-    return predictions, prediction_probabilities
+def main():
+    modeles = charger_modeles()
+    configurer_interface_utilisateur()
+    option = st.radio("Méthode de saisie des données:", ['Remplir manuellement les caractéristiques', 'Télécharger un fichier CSV'])
 
-# Display prediction results
-if st.button('Prédire'):
-    if 'input_df' not in locals():  # Build input_df from feature_values if not from CSV
-        input_df = pd.DataFrame([feature_values])
-    predictions, prediction_probabilities = preprocess_and_predict(input_df, model_choice)
-    
-    # Display the prediction result
-    result = "Normal" if predictions[0] == 0 else "Attaque"
-    st.write(f"Le modèle **{model_choice}** prédit que le trafic est **{result}** avec une probabilité de **{prediction_probabilities[0] * 100:.2f}%**.")
+    input_df = None
+    if option == 'Télécharger un fichier CSV':
+        input_df = upload_csv_interface()
+    elif option == 'Remplir manuellement les caractéristiques':
+        input_df = saisie_manuelle_caracteristiques()
+
+    if input_df is not None:
+        input_df = pretraiter_donnees_entree(input_df)
+        predictions, proba_predictions, modele_choisi = predire_avec_meilleur_modele(modeles, input_df)
+        # Déterminer le résultat de la prédiction et sa couleur en fonction du résultat
+        resultat_prediction = 'une Attaque' if predictions[0] == 1 else 'Normal'
+        couleur_resultat = 'red' if predictions[0] == 1 else 'green'
+
+        # Construire le message HTML avec le style souhaité
+        message_html = f"""
+            <h3 style='color:{couleur_resultat};'>Le système prédit que le trafic est <strong>{resultat_prediction}</strong>.</h3>
+        """
+        st.markdown(message_html, unsafe_allow_html=True)
+        
+if __name__ == '__main__':
+    main()
